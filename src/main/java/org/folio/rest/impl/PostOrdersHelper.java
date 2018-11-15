66e69bacde9e9db832098cc20f4064de7cbcd541
Varun Javalkar <javalkar.varun@gmail.com>
2018-11-15 15:58:50 -0500

    Remove renewal, license so create does not break (#18)
    * Add renewal and license to removal list so create does not break

--- src/main/java/org/folio/rest/impl/PostOrdersHelper.java
+++ src/main/java/org/folio/rest/impl/PostOrdersHelper.java
@@ -116,7 +116,9 @@ public class PostOrdersHelper {
     line.remove("claims");
     line.remove("fund_distribution");
     line.remove("reporting_codes");
-    line.remove("source");    
+    line.remove("source");
+    line.remove("renewal"); 
+    line.remove("license"); 
     
     subObjFuts.add(createAdjustment(compPOL, line, compPOL.getAdjustment()));
     subObjFuts.add(createCost(compPOL, line, compPOL.getCost()));
