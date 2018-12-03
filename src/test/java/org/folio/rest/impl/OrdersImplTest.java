6be397bdce3ec211ad7c89222649b7eaa710406f
KVupp <41596468+KVupp@users.noreply.github.com>
2018-12-03 15:09:35 -0500

    Resolving merge conflicts

--- src/test/java/org/folio/rest/impl/OrdersImplTest.java
+++ src/test/java/org/folio/rest/impl/OrdersImplTest.java
@@ -375,6 +375,26 @@ public class OrdersImplTest {
   }
   
   @Test
+  public void putOrdersById(TestContext ctx) throws Exception {
+    logger.info("=== Test Put Order By Id ===");
+
+    JsonObject ordersList = new JsonObject(getMockData(GetOrdersHelper.MOCK_DATA_PATH));
+    String id = ordersList.getJsonArray("composite_purchase_orders").getJsonObject(0).getString("id");
+    logger.info(String.format("using mock datafile: %s%s.json", BASE_MOCK_DATA_PATH, id));
+    String body = getMockData(listedPrintMonographPath);
+   RestAssured
+      .with()
+        .header(X_OKAPI_URL)
+        .header(X_OKAPI_TENANT)
+        .contentType(APPLICATION_JSON)
+        .body(body)
+      .put(rootPath + "/" + id)
+        .then()
+          .statusCode(204);
+     
+  }
+  
+  @Test
   public void testValidationOnPost() throws Exception {
     logger.info("=== Test validation Annotation on POST API ===");
 
