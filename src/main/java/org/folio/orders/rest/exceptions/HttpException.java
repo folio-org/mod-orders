cd29fb6045c37ef34ce89863168d32df4ca97dcc
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2019-02-08 16:19:21 +0300

    added error code mapping. added default errorCode

--- src/main/java/org/folio/orders/rest/exceptions/HttpException.java
+++ src/main/java/org/folio/orders/rest/exceptions/HttpException.java
@@ -5,39 +5,52 @@ import org.folio.orders.utils.ErrorCodes;
 public class HttpException extends Exception {
   private static final long serialVersionUID = 8109197948434861504L;
 
+  private static final String GENERIC_ERROR_CODE = "genericError";
   private final int code;
+  private final String errorCode;
 
   public HttpException(int code) {
     this.code = code;
+    this.errorCode = GENERIC_ERROR_CODE;
   }
 
   public HttpException(int code, String message) {
     super(message);
     this.code = code;
+    this.errorCode = GENERIC_ERROR_CODE;
   }
 
   public HttpException(int code, ErrorCodes errCodes) {
     super(errCodes.getDescription());
+    this.errorCode = errCodes.getCode();
     this.code = code;
   }
 
   public HttpException(int code, Throwable cause) {
     super(cause);
     this.code = code;
+    this.errorCode = GENERIC_ERROR_CODE;
   }
 
   public HttpException(int code, String message, Throwable cause) {
     super(message, cause);
     this.code = code;
+    this.errorCode = GENERIC_ERROR_CODE;
   }
 
   public HttpException(int code, String message, Throwable cause,
-      boolean enableSuppression, boolean writableStackTrace) {
+                       boolean enableSuppression, boolean writableStackTrace) {
     super(message, cause, enableSuppression, writableStackTrace);
     this.code = code;
+    this.errorCode = GENERIC_ERROR_CODE;
+
   }
 
   public int getCode() {
     return code;
+  }
+
+  public String getErrorCode() {
+    return errorCode;
   }
 }
