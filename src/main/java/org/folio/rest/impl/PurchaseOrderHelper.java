2d544d9d5f367d1ed1a97107dd40cb74995c57f8
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2020-05-12 13:37:06 +0300

    [MODORDERS-390] Populate titleId during piece creation (#296)
    * [MODORDERS-390] Populate titleId during piece creation

--- src/main/java/org/folio/rest/impl/PurchaseOrderHelper.java
+++ src/main/java/org/folio/rest/impl/PurchaseOrderHelper.java
@@ -30,7 +30,6 @@ import static org.folio.orders.utils.HelperUtils.getPurchaseOrderById;
 import static org.folio.orders.utils.HelperUtils.handleDeleteRequest;
 import static org.folio.orders.utils.HelperUtils.handleGetRequest;
 import static org.folio.orders.utils.HelperUtils.handlePutRequest;
-import static org.folio.orders.utils.HelperUtils.verifyNonPackageTitles;
 import static org.folio.orders.utils.HelperUtils.verifyProtectedFieldsChanged;
 import static org.folio.orders.utils.POProtectedFields.getFieldNames;
 import static org.folio.orders.utils.POProtectedFields.getFieldNamesForOpenOrder;
@@ -427,6 +426,8 @@ public class PurchaseOrderHelper extends AbstractHelper {
       .thenApply(HelperUtils::convertToCompositePurchaseOrder)
       .thenAccept(compPO -> protectionHelper.isOperationRestricted(compPO.getAcqUnitIds(), ProtectedOperationType.READ)
         .thenAccept(ok -> getOrderWithLines(compPO)
+          .thenCompose(this::fetchNonPackageTitles)
+          .thenAccept(linesIdTitles -> populateInstanceId(linesIdTitles, compPO.getCompositePoLines()))
           .thenApply(v -> populateOrderSummary(compPO))
           .thenAccept(future::complete)
           .exceptionally(t -> {
@@ -469,9 +470,14 @@ public class PurchaseOrderHelper extends AbstractHelper {
   public CompletableFuture<Void> openOrder(CompositePurchaseOrder compPO) {
     compPO.setWorkflowStatus(OPEN);
     compPO.setDateOrdered(new Date());
-    return getOrderWithLines(compPO).thenApply(this::validateFundDistributionTotal)
+    return getOrderWithLines(compPO)
+      .thenApply(this::validateFundDistributionTotal)
       .thenApply(this::validateMaterialTypes)
-      .thenCompose(this::updateInventory)
+      .thenCompose(this::fetchNonPackageTitles)
+      .thenCompose(linesIdTitles -> {
+          populateInstanceId(linesIdTitles, compPO.getCompositePoLines());
+          return updateInventory(linesIdTitles, compPO);
+        })
       .thenCompose(ok -> createEncumbrances(compPO))
       .thenAccept(ok -> changePoLineStatuses(compPO))
       .thenCompose(ok -> updatePoLinesSummary(compPO));
@@ -758,8 +764,6 @@ public class PurchaseOrderHelper extends AbstractHelper {
           orderLineHelper.sortPoLinesByPoLineNumber(poLines);
           return compPO.withCompositePoLines(poLines);
         })
-        .thenCompose(this::fetchNonPackageTitles)
-        .thenAccept(linesIdTitles -> populateInstanceId(linesIdTitles, compPO.getCompositePoLines()))
         .thenApply(v -> compPO);
     } else {
       return completedFuture(compPO);
@@ -893,13 +897,20 @@ public class PurchaseOrderHelper extends AbstractHelper {
     return compPO.getCompositePoLines().stream().mapToLong(compositePoLine -> compositePoLine.getFundDistribution().size()).sum() >= 1;
   }
 
-  private CompletableFuture<Void> updateInventory(CompositePurchaseOrder compPO) {
+  private CompletableFuture<Void> updateInventory(Map<String, List<Title>> lineIdsTitles, CompositePurchaseOrder compPO) {
     return CompletableFuture.allOf(
       compPO.getCompositePoLines()
-            .stream()
-            .map(orderLineHelper::updateInventory)
-            .toArray(CompletableFuture[]::new)
+        .stream()
+        .map(poLine -> orderLineHelper.updateInventory(poLine, getFirstTitleIdIfExist(lineIdsTitles, poLine)))
+        .toArray(CompletableFuture[]::new)
     );
+  }
+
+  private String getFirstTitleIdIfExist(Map<String, List<Title>> lineIdsTitles, CompositePoLine poLine) {
+    return Optional.ofNullable(lineIdsTitles.get(poLine.getId()))
+      .map(titles -> titles.get(0))
+      .map(Title::getId)
+      .orElse(null);
   }
 
   private CompletableFuture<Void> handlePoLines(CompositePurchaseOrder compOrder, List<PoLine> poLinesFromStorage) {
