3f47a55f36ffb2b3343e656f3f0266a36d565875
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2020-07-07 16:34:23 +0300

    MODORDERS-402 missed permissions

--- src/main/java/org/folio/rest/impl/FinanceHelper.java
+++ src/main/java/org/folio/rest/impl/FinanceHelper.java
@@ -546,16 +546,22 @@ public class FinanceHelper extends AbstractHelper {
   }
 
   CompletionStage<Void> releasePoLineEncumbrances(String lineId) {
-    return getPoLineEncumbrances(lineId)
-      .thenCompose(trs -> updateOrderTransactionSummary(trs.get(0).getEncumbrance().getSourcePurchaseOrderId(), trs.size())
-        .thenApply(v -> trs))
-      .thenCompose(transactionService::releaseEncumbrances);
+    return getPoLineEncumbrances(lineId).thenCompose(trs -> {
+      if (!trs.isEmpty()) {
+        return updateOrderTransactionSummary(trs.get(0).getEncumbrance().getSourcePurchaseOrderId(), trs.size())
+          .thenCompose(v -> transactionService.releaseEncumbrances(trs));
+      }
+      return CompletableFuture.completedFuture(null);
+    });
   }
 
   CompletionStage<Void> releaseOrderEncumbrances(String orderId) {
-    return getOrderEncumbrances(orderId)
-      .thenCompose(trs -> updateOrderTransactionSummary(orderId, trs.size())
-        .thenApply(v -> trs))
-      .thenCompose(transactionService::releaseEncumbrances);
+    return getOrderEncumbrances(orderId).thenCompose(trs -> {
+      if (!trs.isEmpty()) {
+        return updateOrderTransactionSummary(orderId, trs.size())
+          .thenCompose(v -> transactionService.releaseEncumbrances(trs));
+      }
+      return CompletableFuture.completedFuture(null);
+    });
   }
 }
