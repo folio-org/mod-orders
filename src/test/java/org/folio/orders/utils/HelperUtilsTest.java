2900844b945aa83be74458e31b8b6201c416b098
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2021-01-29 17:53:13 +0300

    MODORDERS-443 Update logic for supporting manually exchange rate for purchase order line
    Signed-off-by: Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>

--- src/test/java/org/folio/orders/utils/HelperUtilsTest.java
+++ src/test/java/org/folio/orders/utils/HelperUtilsTest.java
@@ -1,8 +1,11 @@
 package org.folio.orders.utils;
 
+import static org.folio.service.exchange.ExchangeRateProviderResolver.RATE_KEY;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 
+import javax.money.convert.ConversionQuery;
 import org.junit.jupiter.api.Test;
 
 public class HelperUtilsTest {
@@ -11,5 +14,13 @@ public class HelperUtilsTest {
   public void testShouldReturnEmptyString(){
     String act = HelperUtils.combineCqlExpressions("");
     assertThat(act, isEmptyOrNullString());
+  }
+
+  @Test
+  public void testShouldReturnConversionQueryWithRateKeyForGetConversionQueryWithExchangeRate(){
+    ConversionQuery conversionQuery = HelperUtils.getConversionQuery(2.0d, "USD", "EUR");
+    assertThat(conversionQuery.get(RATE_KEY, Double.class), is(2.0d));
+    assertThat(conversionQuery.getBaseCurrency().getCurrencyCode(), is("USD"));
+    assertThat(conversionQuery.getCurrency().getCurrencyCode(), is("EUR"));
   }
 }
