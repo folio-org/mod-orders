bf265b3bac43303c8d98dab3868cb52fd26d6c2a
MaksymShtanko <maksym_shtanko@epam.com>
2021-04-22 18:01:32 +0300

    [MODORDERS-224] Minor refactor.

--- src/test/java/org/folio/orders/utils/validators/OngoingOrderValidatorTest.java
+++ src/test/java/org/folio/orders/utils/validators/OngoingOrderValidatorTest.java
@@ -1,26 +1,27 @@
 package org.folio.orders.utils.validators;
 
-import io.vertx.core.json.JsonObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.folio.orders.rest.exceptions.HttpException;
-import org.folio.rest.jaxrs.model.CompositePurchaseOrder;
-import org.folio.rest.jaxrs.model.Ongoing;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
-
-import java.io.IOException;
-
 import static org.folio.TestConstants.COMP_ORDER_MOCK_DATA_PATH;
 import static org.folio.TestConstants.ID;
 import static org.folio.TestUtils.getMockAsJson;
 import static org.folio.TestUtils.getMockData;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
+import java.io.IOException;
+import java.util.Date;
+
+import org.folio.orders.rest.exceptions.HttpException;
+import org.folio.rest.jaxrs.model.CompositePurchaseOrder;
+import org.folio.rest.jaxrs.model.Ongoing;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import io.vertx.core.json.JsonObject;
+
+@DisplayName("OngoingOrderValidator class ")
 public class OngoingOrderValidatorTest {
-
-  private static final Logger LOGGER = LogManager.getLogger();
 
   private static final String ORDERS_MOCK_DATA_PATH = COMP_ORDER_MOCK_DATA_PATH + "getOrders.json";
 
@@ -30,41 +31,52 @@ public class OngoingOrderValidatorTest {
   }
 
   @Test
+  @DisplayName("should return http exception on renewal date is not set")
   void testShouldThrowHttpExceptionOnRenewalDateIsNotSet() throws IOException {
-    LOGGER.info("Test should return http exception on renewal date is not set.");
-
     CompositePurchaseOrder compositePurchaseOrder = getCompositePurchaseOrder();
 
     Ongoing ongoing = new org.folio.rest.jaxrs.model.Ongoing();
+    ongoing.setInterval(0);
     ongoing.setRenewalDate(null);
 
     compositePurchaseOrder.setOrderType(CompositePurchaseOrder.OrderType.ONGOING);
     compositePurchaseOrder.setOngoing(ongoing);
 
-    Assertions.assertThrows(HttpException.class, () -> OngoingOrderValidator.validate(compositePurchaseOrder));
+    try {
+      OngoingOrderValidator.validate(compositePurchaseOrder);
+    } catch (HttpException e) {
+      assertThat(e.getError().getCode(), is("renewalDateIsNotSet"));
+      assertThat(e.getError().getMessage(), is("Renewal date is not set"));
+    }
   }
 
   @Test
+  @DisplayName("should return http exception on renewal interval is not set")
   void shouldThrowHttpExceptionOnRenewalIntervalIsNotSet() throws IOException {
-    LOGGER.info("Test should return http exception on renewal interval is not set.");
-
     CompositePurchaseOrder compositePurchaseOrder = getCompositePurchaseOrder();
 
     Ongoing ongoing = new org.folio.rest.jaxrs.model.Ongoing();
+    ongoing.setRenewalDate(new Date());
     ongoing.setInterval(null);
 
     compositePurchaseOrder.setOrderType(CompositePurchaseOrder.OrderType.ONGOING);
     compositePurchaseOrder.setOngoing(ongoing);
 
-    Assertions.assertThrows(HttpException.class, () -> OngoingOrderValidator.validate(compositePurchaseOrder));
+    try {
+      OngoingOrderValidator.validate(compositePurchaseOrder);
+    } catch (HttpException e) {
+      assertThat(e.getError().getCode(), is("renewalIntervalIsNotSet"));
+      assertThat(e.getError().getMessage(), is("Renewal interval is not set"));
+    }
   }
 
   CompositePurchaseOrder getCompositePurchaseOrder() throws IOException {
     JsonObject ordersList = new JsonObject(getMockData(ORDERS_MOCK_DATA_PATH));
-    String id = ordersList.getJsonArray("compositePurchaseOrders").getJsonObject(0).getString(ID);
+    String id = ordersList.getJsonArray("compositePurchaseOrders")
+      .getJsonObject(0)
+      .getString(ID);
     var order = getMockAsJson(COMP_ORDER_MOCK_DATA_PATH, id).mapTo(CompositePurchaseOrder.class);
 
     return order;
   }
 }
-
