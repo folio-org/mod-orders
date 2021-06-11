12f01adcea5d7166427eee54a2a8de60d8c25f1b
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2021-06-11 13:24:29 +0300

    [MODORDERS-516] - Consider exchange rate when updating rollover adjus… (#456)
    * [MODORDERS-516] - Consider exchange rate when updating rollover adjustment amount in POL
    
    * [MODORDERS-516] - Unit tests for exchange rate added
    
    * [MODORDERS-516] - Update permissions for rollover

--- src/test/java/org/folio/orders/utils/HelperUtilsTest.java
+++ src/test/java/org/folio/orders/utils/HelperUtilsTest.java
@@ -4,9 +4,15 @@ import static org.folio.service.exchange.ExchangeRateProviderResolver.RATE_KEY;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import javax.money.convert.ConversionQuery;
+import org.folio.rest.jaxrs.model.Cost;
+import org.folio.rest.jaxrs.model.PoLine;
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.UUID;
 
 public class HelperUtilsTest {
 
@@ -22,5 +28,26 @@ public class HelperUtilsTest {
     assertThat(conversionQuery.get(RATE_KEY, Double.class), is(2.0d));
     assertThat(conversionQuery.getBaseCurrency().getCurrencyCode(), is("USD"));
     assertThat(conversionQuery.getCurrency().getCurrencyCode(), is("EUR"));
+  }
+
+  @Test
+  public void testShouldBuildQueryWithoutExchangeRate(){
+    String systemCurrency = "USD";
+    Cost costOneTime = new Cost().withListUnitPrice(595d).withQuantityPhysical(1).withCurrency("EUR").withPoLineEstimatedPrice(595d);
+    PoLine poLineOneTime = new PoLine().withId(UUID.randomUUID().toString()).withPurchaseOrderId(UUID.randomUUID().toString()).withCost(costOneTime);
+    ConversionQuery actQuery = HelperUtils.buildConversionQuery(poLineOneTime, systemCurrency);
+    assertEquals(actQuery.getCurrency().getCurrencyCode(), systemCurrency);
+    assertNull(actQuery.get(RATE_KEY, Double.class));
+  }
+
+  @Test
+  public void testShouldBuildQueryWithExchangeRate(){
+    String systemCurrency = "USD";
+    Cost costOneTime = new Cost().withListUnitPrice(595d).withQuantityPhysical(1).withExchangeRate(2d).withCurrency("EUR").withPoLineEstimatedPrice(595d);
+    PoLine poLineOneTime = new PoLine().withId(UUID.randomUUID().toString()).withPurchaseOrderId(UUID.randomUUID().toString()).withCost(costOneTime);
+    ConversionQuery actQuery = HelperUtils.buildConversionQuery(poLineOneTime, systemCurrency);
+
+    assertEquals(actQuery.getCurrency().getCurrencyCode(), systemCurrency);
+    assertEquals(Double.valueOf(2d), actQuery.get(RATE_KEY, Double.class));
   }
 }
