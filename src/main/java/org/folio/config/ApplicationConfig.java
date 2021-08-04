c230998fc97c78d64a62e51dc660d2dcbc4b8ef8
Siarhei Hrabko <grabsefx@gmail.com>
2021-08-04 15:36:44 +0300

    MODORDERS-544 Unopen fails after a rollover for an order created in the previous year

--- src/main/java/org/folio/config/ApplicationConfig.java
+++ src/main/java/org/folio/config/ApplicationConfig.java
@@ -157,8 +157,11 @@ public class ApplicationConfig {
   }
 
   @Bean
-  EncumbranceWorkflowStrategy openToPendingEncumbranceStrategy(EncumbranceService encumbranceService, TransactionSummariesService transactionSummariesService) {
-    return new OpenToPendingEncumbranceStrategy(encumbranceService, transactionSummariesService);
+  EncumbranceWorkflowStrategy openToPendingEncumbranceStrategy(EncumbranceService encumbranceService,
+      TransactionSummariesService transactionSummariesService,
+      EncumbranceRelationsHoldersBuilder encumbranceRelationsHoldersBuilder) {
+    return new OpenToPendingEncumbranceStrategy(encumbranceService, transactionSummariesService,
+        encumbranceRelationsHoldersBuilder);
   }
 
   @Bean
