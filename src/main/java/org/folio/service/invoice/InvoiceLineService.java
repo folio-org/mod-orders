0c18f76469c7b08eff3cb39d848bcbb3e80e7c1c
Oleksandr_Oliinyk2 <oleksandr_oliinyk2@epam.com>
2021-09-09 09:39:43 +0300

    Cannot delete PO Line if other PO Line on the same order is linked to an invoice line

--- src/main/java/org/folio/service/invoice/InvoiceLineService.java
+++ src/main/java/org/folio/service/invoice/InvoiceLineService.java
@@ -1,6 +1,5 @@
 package org.folio.service.invoice;
 
-import one.util.streamex.StreamEx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.acq.model.invoice.InvoiceLine;
@@ -9,14 +8,8 @@ import org.folio.rest.core.RestClient;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.core.models.RequestEntry;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-
-import static java.util.stream.Collectors.toList;
-import static org.folio.helper.AbstractHelper.MAX_IDS_FOR_GET_RQ;
-import static org.folio.orders.utils.HelperUtils.collectResultsOnSuccess;
-import static org.folio.orders.utils.HelperUtils.convertFieldListToCqlQuery;
 
 public class InvoiceLineService {
 
@@ -35,21 +28,6 @@ public class InvoiceLineService {
       .withLimit(Integer.MAX_VALUE)
       .withOffset(0);
     return restClient.get(requestEntry, requestContext, InvoiceLineCollection.class);
-  }
-
-  public CompletableFuture<List<InvoiceLine>> getInvoiceLinesByIds(List<String> invoiceIds, RequestContext requestContext) {
-    List<CompletableFuture<List<InvoiceLine>>> futures = StreamEx
-      .ofSubLists(invoiceIds, MAX_IDS_FOR_GET_RQ)
-      .map(ids -> getInvoiceLinesChunk(ids, requestContext))
-      .collect(toList());
-    return collectResultsOnSuccess(futures)
-      .thenApply(listList -> listList.stream().flatMap(Collection::stream).distinct().collect(toList()));
-  }
-
-  private CompletableFuture<List<InvoiceLine>> getInvoiceLinesChunk(List<String> ids, RequestContext requestContext) {
-    String query = convertFieldListToCqlQuery(ids, "invoiceId", true);
-    return getInvoiceLines(query, 0, Integer.MAX_VALUE, requestContext)
-      .thenApply(InvoiceLineCollection::getInvoiceLines);
   }
 
   public CompletableFuture<List<InvoiceLine>> getInvoiceLinesByOrderLineId(String orderPoLineId, RequestContext requestContext) {
