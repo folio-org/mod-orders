d3f78bbea1695137a2a0dbff7ad6ad13340cd5d9
Damien <damien-git@users.noreply.github.com>
2021-10-06 14:06:14 -0400

    [MODORDERS-528] Avoid creating inventory records when open order fails because of encumbrances

--- src/main/java/org/folio/service/finance/transaction/PendingToOpenEncumbranceStrategy.java
+++ src/main/java/org/folio/service/finance/transaction/PendingToOpenEncumbranceStrategy.java
@@ -14,42 +14,59 @@ import org.folio.service.orders.OrderWorkflowType;
 
 public class PendingToOpenEncumbranceStrategy implements EncumbranceWorkflowStrategy {
 
-    private final EncumbranceService encumbranceService;
-    private final FundsDistributionService fundsDistributionService;
-    private final BudgetRestrictionService budgetRestrictionService;
-    private final EncumbranceRelationsHoldersBuilder encumbranceRelationsHoldersBuilder;
-    private final EncumbrancesProcessingHolderBuilder encumbrancesProcessingHolderBuilder;
+  private final EncumbranceService encumbranceService;
+  private final FundsDistributionService fundsDistributionService;
+  private final BudgetRestrictionService budgetRestrictionService;
+  private final EncumbranceRelationsHoldersBuilder encumbranceRelationsHoldersBuilder;
+  private final EncumbrancesProcessingHolderBuilder encumbrancesProcessingHolderBuilder;
+  private List<EncumbranceRelationsHolder> encumbranceRelationsHolders;
+  private boolean prepared;
 
-    public PendingToOpenEncumbranceStrategy(EncumbranceService encumbranceService, FundsDistributionService fundsDistributionService,
+  public PendingToOpenEncumbranceStrategy(EncumbranceService encumbranceService, FundsDistributionService fundsDistributionService,
       BudgetRestrictionService budgetRestrictionService, EncumbranceRelationsHoldersBuilder encumbranceRelationsHoldersBuilder,
       EncumbrancesProcessingHolderBuilder encumbrancesProcessingHolderBuilder) {
-        this.encumbranceService = encumbranceService;
-        this.fundsDistributionService = fundsDistributionService;
-        this.budgetRestrictionService = budgetRestrictionService;
-        this.encumbranceRelationsHoldersBuilder = encumbranceRelationsHoldersBuilder;
-        this.encumbrancesProcessingHolderBuilder = encumbrancesProcessingHolderBuilder;
-    }
+    this.encumbranceService = encumbranceService;
+    this.fundsDistributionService = fundsDistributionService;
+    this.budgetRestrictionService = budgetRestrictionService;
+    this.encumbranceRelationsHoldersBuilder = encumbranceRelationsHoldersBuilder;
+    this.encumbrancesProcessingHolderBuilder = encumbrancesProcessingHolderBuilder;
+    prepared = false;
+  }
 
-    @Override
-    public CompletableFuture<Void> processEncumbrances(CompositePurchaseOrder compPO, CompositePurchaseOrder poAndLinesFromStorage,
-        RequestContext requestContext) {
+  @Override
+  public CompletableFuture<Void> prepareProcessEncumbrancesAndValidate(CompositePurchaseOrder compPO,
+      CompositePurchaseOrder poAndLinesFromStorage, RequestContext requestContext) {
+    validateFundDistributionTotal(compPO.getCompositePoLines());
+    encumbranceRelationsHolders = encumbranceRelationsHoldersBuilder.buildBaseHolders(compPO);
+    return encumbranceRelationsHoldersBuilder.withBudgets(encumbranceRelationsHolders, requestContext)
+      .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withLedgersData(holders, requestContext))
+      .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withFiscalYearData(holders, requestContext))
+      .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withConversion(holders, requestContext))
+      .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withExistingTransactions(holders, poAndLinesFromStorage, requestContext))
+      .thenApply(fundsDistributionService::distributeFunds)
+      .thenAccept(budgetRestrictionService::checkEncumbranceRestrictions)
+      .thenAccept(v -> {
+        prepared = true;
+      });
+  }
 
-      validateFundDistributionTotal(compPO.getCompositePoLines());
-      List<EncumbranceRelationsHolder> encumbranceRelationsHolders = encumbranceRelationsHoldersBuilder
-        .buildBaseHolders(compPO);
-      return encumbranceRelationsHoldersBuilder.withBudgets(encumbranceRelationsHolders, requestContext)
-        .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withLedgersData(holders, requestContext))
-        .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withFiscalYearData(holders, requestContext))
-        .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withConversion(holders, requestContext))
-        .thenCompose(holders -> encumbranceRelationsHoldersBuilder.withExistingTransactions(holders, poAndLinesFromStorage, requestContext))
-        .thenApply(fundsDistributionService::distributeFunds)
-        .thenAccept(budgetRestrictionService::checkEncumbranceRestrictions)
-        .thenApply(aVoid -> encumbrancesProcessingHolderBuilder.distributeHoldersByOperation(encumbranceRelationsHolders))
-        .thenCompose(holder -> encumbranceService.createOrUpdateEncumbrances(holder, requestContext));
-    }
+  @Override
+  public CompletableFuture<Void> processEncumbrances(CompositePurchaseOrder compPO, CompositePurchaseOrder poAndLinesFromStorage,
+      RequestContext requestContext) {
+    CompletableFuture<Void> f;
+    if (prepared)
+      f = CompletableFuture.completedFuture(null);
+    else
+      f = prepareProcessEncumbrancesAndValidate(compPO, poAndLinesFromStorage, requestContext);
+    return f.thenApply(v -> encumbrancesProcessingHolderBuilder.distributeHoldersByOperation(encumbranceRelationsHolders))
+      .thenCompose(holder -> encumbranceService.createOrUpdateEncumbrances(holder, requestContext))
+      .whenComplete((v, t) -> {
+        prepared = false;
+      });
+  }
 
-    @Override
-    public OrderWorkflowType getStrategyName() {
-      return OrderWorkflowType.PENDING_TO_OPEN;
-    }
+  @Override
+  public OrderWorkflowType getStrategyName() {
+    return OrderWorkflowType.PENDING_TO_OPEN;
+  }
 }
