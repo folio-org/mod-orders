53ead39cec37fca0b60b5098183bfb2019851f84
Serhii Nosko <Serhii_Nosko@epam.com>
2022-07-18 10:51:26 +0300

    [MODORDERS-708]. Provide endpoint to validate fund distributions for a po line

--- src/main/java/org/folio/orders/utils/FundDistributionUtils.java
+++ src/main/java/org/folio/orders/utils/FundDistributionUtils.java
@@ -1,8 +1,11 @@
 package org.folio.orders.utils;
 
+import com.google.common.collect.Lists;
 import static java.math.RoundingMode.HALF_EVEN;
+import org.apache.commons.collections4.CollectionUtils;
 import static org.folio.rest.core.exceptions.ErrorCodes.CANNOT_MIX_TYPES_FOR_ZERO_PRICE;
 import static org.folio.rest.core.exceptions.ErrorCodes.INCORRECT_FUND_DISTRIBUTION_TOTAL;
+import org.folio.rest.jaxrs.model.Cost;
 import static org.folio.rest.jaxrs.model.FundDistribution.DistributionType.AMOUNT;
 import static org.folio.rest.jaxrs.model.FundDistribution.DistributionType.PERCENTAGE;
 
@@ -12,8 +15,11 @@ import java.util.List;
 import org.folio.rest.core.exceptions.HttpException;
 import org.folio.rest.jaxrs.model.CompositePoLine;
 import org.folio.rest.jaxrs.model.FundDistribution;
+import org.folio.rest.jaxrs.model.Parameter;
 
 public final class FundDistributionUtils {
+
+  public static final String REMAINING_AMOUNT_FIELD = "remainingAmount";
 
   private FundDistributionUtils() {
 
@@ -21,32 +27,36 @@ public final class FundDistributionUtils {
 
   public static void validateFundDistributionTotal(List<CompositePoLine> compositePoLines) {
     for (CompositePoLine cPoLine : compositePoLines) {
+      validateFundDistributionTotal(cPoLine.getCost(), cPoLine.getFundDistribution());
+    }
+  }
 
-      if (cPoLine.getCost().getPoLineEstimatedPrice() != null && !cPoLine.getFundDistribution().isEmpty()) {
-        Double poLineEstimatedPrice = cPoLine.getCost().getPoLineEstimatedPrice();
-        if (poLineEstimatedPrice == 0d) {
-          validateZeroPrice(cPoLine.getFundDistribution());
-          continue;
-        }
-        BigDecimal remainingPercent = BigDecimal.valueOf(100);
+  public static void validateFundDistributionTotal(Cost cost, List<FundDistribution> fundDistributions) {
+    if (cost.getPoLineEstimatedPrice() != null && CollectionUtils.isNotEmpty(fundDistributions)) {
+      Double poLineEstimatedPrice = cost.getPoLineEstimatedPrice();
+      if (poLineEstimatedPrice == 0d) {
+        validateZeroPrice(fundDistributions);
+        return;
+      }
+      BigDecimal remainingPercent = BigDecimal.valueOf(100);
 
-        for (FundDistribution fundDistribution : cPoLine.getFundDistribution()) {
+      for (FundDistribution fundDistribution : fundDistributions) {
 
-          FundDistribution.DistributionType dType = fundDistribution.getDistributionType();
-          if (dType == PERCENTAGE) {
-            remainingPercent = remainingPercent.subtract(BigDecimal.valueOf(fundDistribution.getValue()));
-          } else {
-            Double value = fundDistribution.getValue();
-            BigDecimal percentageValue = BigDecimal.valueOf(value)
-                .divide(BigDecimal.valueOf(poLineEstimatedPrice), 15, HALF_EVEN)
-                .movePointRight(2);
-            remainingPercent = remainingPercent.subtract(percentageValue);
-          }
+        FundDistribution.DistributionType dType = fundDistribution.getDistributionType();
+        if (dType == PERCENTAGE) {
+          remainingPercent = remainingPercent.subtract(BigDecimal.valueOf(fundDistribution.getValue()));
+        } else {
+          Double value = fundDistribution.getValue();
+          BigDecimal percentageValue = BigDecimal.valueOf(value)
+            .divide(BigDecimal.valueOf(poLineEstimatedPrice), 15, HALF_EVEN)
+            .movePointRight(2);
+          remainingPercent = remainingPercent.subtract(percentageValue);
         }
-        BigDecimal epsilon = BigDecimal.valueOf(1e-10);
-        if (remainingPercent.abs().compareTo(epsilon) > 0) {
-          throw new HttpException(422, INCORRECT_FUND_DISTRIBUTION_TOTAL);
-        }
+      }
+      BigDecimal epsilon = BigDecimal.valueOf(1e-10);
+      if (remainingPercent.abs().compareTo(epsilon) > 0) {
+        throw new HttpException(422, INCORRECT_FUND_DISTRIBUTION_TOTAL,
+          Lists.newArrayList(new Parameter().withKey(REMAINING_AMOUNT_FIELD).withValue(remainingPercent.toString())));
       }
     }
   }
