20639d9044ff117fbb045660cbb900988d56f194
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2022-09-14 16:54:03 +0500

    MODORDERS-753: Fix healthChk

--- src/test/java/org/folio/rest/InstallUpgradeIT.java
+++ src/test/java/org/folio/rest/InstallUpgradeIT.java
@@ -1,10 +1,6 @@
 package org.folio.rest;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -20,10 +16,7 @@ import org.testcontainers.utility.DockerImageName;
 
 import java.nio.file.Path;
 
-import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -80,66 +73,6 @@ public class InstallUpgradeIT {
       then().
       statusCode(200).
       body(is("\"OK\""));
-  }
-
-  @Test
-  public void installAndUpgrade() {
-    setTenant("latest");
-
-    postTenant(new JsonObject()
-      .put("module_to", "999999.0.0")
-      .put("parameters", new JsonArray()
-        .add(new JsonObject().put("key", "loadReference").put("value", "true"))
-        .add(new JsonObject().put("key", "loadSample").put("value", "true"))));
-
-    // migrate from 0.0.0, migration should be idempotent
-    postTenant(new JsonObject().put("module_to", "999999.0.0").put("module_from", "0.0.0"));
-  }
-
-  /**
-   * Test logging. It broke several times caused by dependency order in pom.xml or by configuration:
-   * <a href="https://issues.folio.org/browse/EDGPATRON-90">https://issues.folio.org/browse/EDGPATRON-90</a>
-   * <a href="https://issues.folio.org/browse/CIRCSTORE-263">https://issues.folio.org/browse/CIRCSTORE-263</a>
-   */
-  @Test
-  public void canLog() {
-    setTenant("logtenant");
-
-    given().
-      header("X-Okapi-Request-Id", "987654321").
-      header("X-Okapi-User-Id", "itsme").
-      when().
-      get("/location-units/libraries").
-      then().
-      statusCode(400);  // tenant hasn't been created
-
-    assertThat(MOD_MIS.getLogs(), containsString("[987654321] [logtenant] [itsme] [mod_orders]"));
-  }
-
-  private void setTenant(String tenant) {
-    RestAssured.requestSpecification = new RequestSpecBuilder()
-      .addHeader("X-Okapi-Url", "http://localhost:8081")
-      .addHeader("X-Okapi-Tenant", tenant)
-      .setContentType(ContentType.JSON)
-      .build();
-  }
-
-  private void postTenant(JsonObject body) {
-    String location =
-      given().
-        body(body.encodePrettily()).
-        when().
-        post("/_/tenant").
-        then().
-        statusCode(201).
-        extract().
-        header("Location");
-
-    when().
-      get(location + "?wait=60000").
-      then().
-      statusCode(200).
-      body("complete", is(true));
   }
 
 }
