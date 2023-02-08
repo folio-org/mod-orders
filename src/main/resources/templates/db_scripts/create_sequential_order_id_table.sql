38d3a52eee6f647de250a7f4e0ff98da9639585d
Aliaksandr Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2023-02-08 13:55:36 +0500

    MODORDERS-817: Associate mapped POLines with single Order based on POLine limit value (#696)
    * MODORDERS-817: Associate mapped POLines with single Order based on POLine limit value

---
+++ src/main/resources/templates/db_scripts/create_sequential_order_id_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS sequential_order_id (
+  job_execution_id uuid NOT NULL,
+  sequential_no integer NOT NULL,
+  order_id uuid NOT NULL,
+  CONSTRAINT sequential_order_pk_constraint PRIMARY KEY(job_execution_id, sequential_no)
+);
