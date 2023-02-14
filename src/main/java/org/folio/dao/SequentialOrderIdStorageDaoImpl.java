d9f444d2a9a3fd06f7633ca3700a49703c215fdc
Volodymyr_Rohach <Volodymyr_Rohach@epam.com>
2023-02-14 12:29:30 +0200

    MODODERS-843: UUID casting improvements...

--- src/main/java/org/folio/dao/SequentialOrderIdStorageDaoImpl.java
+++ src/main/java/org/folio/dao/SequentialOrderIdStorageDaoImpl.java
@@ -25,7 +25,7 @@ public class SequentialOrderIdStorageDaoImpl implements SequentialOrderIdStorage
   private static final String TABLE_NAME = "sequential_order_id";
 
   private static final String SQL =
-    "WITH input_row(job_execution_id, sequential_no, order_id, saved_timestamp) AS " +
+    "WITH input_row(job_execution_id::uuid, sequential_no, order_id, saved_timestamp) AS " +
       "(VALUES ($1, $2, $3, $4)), " +
       "insert_res AS ( " +
       "  INSERT INTO %1$s (job_execution_id, sequential_no, order_id, saved_timestamp) " +
