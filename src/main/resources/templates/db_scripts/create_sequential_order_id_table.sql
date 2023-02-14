9ea8a5419f5ec81b0120d9242df4955cd341590f
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2023-02-14 17:45:47 +0500

    MODODERS-843: Timestamptz casting added.

--- src/main/resources/templates/db_scripts/create_sequential_order_id_table.sql
+++ src/main/resources/templates/db_scripts/create_sequential_order_id_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS sequential_order_id (
   job_execution_id uuid NOT NULL,
   sequential_no integer NOT NULL,
   order_id uuid NOT NULL,
-  saved_timestamp timestamp,
+  saved_timestamp timestamptz,
   CONSTRAINT sequential_order_pk_constraint PRIMARY KEY(job_execution_id, sequential_no)
 );
