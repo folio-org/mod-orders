85770603999a838f09371a19a88da4f7a32660ff
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2023-03-17 18:34:02 +0500

    [MODORDERS-865] - Increased rollover error service test coverage

--- src/test/java/org/folio/ApiTestSuite.java
+++ src/test/java/org/folio/ApiTestSuite.java
@@ -46,6 +46,7 @@ import org.folio.service.exchange.ManualExchangeRateProviderTest;
 import org.folio.service.expenceclass.ExpenseClassValidationServiceTest;
 import org.folio.service.finance.FundServiceTest;
 import org.folio.service.finance.budget.BudgetRestrictionServiceTest;
+import org.folio.service.finance.rollover.LedgerRolloverErrorServiceTest;
 import org.folio.service.finance.transaction.EncumbranceRelationsHoldersBuilderTest;
 import org.folio.service.finance.transaction.EncumbranceServiceTest;
 import org.folio.service.finance.transaction.OpenToClosedEncumbranceStrategyTest;
@@ -443,4 +444,9 @@ public class ApiTestSuite {
   @Nested
   class ExportHistoryImplTestNested extends ExportHistoryImplTest {
   }
+
+  @Nested
+  class LedgerRolloverErrorServiceTestNested extends LedgerRolloverErrorServiceTest {
+  }
+
 }
