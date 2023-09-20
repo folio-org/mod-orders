2697b33bd0e2f6dceef76af560f3a17c71acf601
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2023-09-21 00:12:06 +0500

    [MODORDERS-907] - Fixed deserialization error

--- src/main/java/org/folio/models/consortium/SharingInstance.java
+++ src/main/java/org/folio/models/consortium/SharingInstance.java
@@ -1,8 +1,11 @@
 package org.folio.models.consortium;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SharingInstance(UUID id,
                               UUID instanceIdentifier,
                               String sourceTenantId,
@@ -11,19 +14,5 @@ public record SharingInstance(UUID id,
                               String error) {
   public SharingInstance(UUID instanceIdentifier, String sourceTenantId, String targetTenantId) {
     this(null, instanceIdentifier, sourceTenantId, targetTenantId, null, null);
-  }
-
-  public SharingInstance(UUID id,
-                         UUID instanceIdentifier,
-                         String sourceTenantId,
-                         String targetTenantId,
-                         SharingStatus status,
-                         String error) {
-    this.id = id;
-    this.instanceIdentifier = instanceIdentifier;
-    this.sourceTenantId = sourceTenantId;
-    this.targetTenantId = targetTenantId;
-    this.status = status;
-    this.error = error;
   }
 }
