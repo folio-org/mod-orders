22be832483756d0a3ae2d10c65a9e167c2ac404a
Damien <damien-git@users.noreply.github.com>
2024-03-01 10:03:34 -0500

    [MODORDERS-1039] Update encumbrance expense class

--- src/main/java/org/folio/service/finance/transaction/PendingToPendingEncumbranceStrategy.java
+++ src/main/java/org/folio/service/finance/transaction/PendingToPendingEncumbranceStrategy.java
@@ -1,6 +1,5 @@
 package org.folio.service.finance.transaction;
 
-import static java.util.stream.Collectors.toList;
 import static org.folio.orders.utils.FundDistributionUtils.validateFundDistributionTotal;
 
 import java.util.List;
@@ -44,8 +43,10 @@ public class PendingToPendingEncumbranceStrategy implements EncumbranceWorkflowS
 
   private EncumbrancesProcessingHolder distributeHoldersByOperation(List<EncumbranceRelationsHolder> encumbranceRelationsHolders) {
     EncumbrancesProcessingHolder holder = new EncumbrancesProcessingHolder();
+    List<EncumbranceRelationsHolder> toUpdate = getTransactionsToUpdate(encumbranceRelationsHolders);
     List<EncumbranceRelationsHolder> toDelete = getTransactionsToDelete(encumbranceRelationsHolders);
-    List<Transaction> toRelease = toDelete.stream().map(EncumbranceRelationsHolder::getOldEncumbrance).collect(toList());
+    List<Transaction> toRelease = toDelete.stream().map(EncumbranceRelationsHolder::getOldEncumbrance).toList();
+    holder.withEncumbrancesForUpdate(toUpdate);
     holder.withEncumbrancesForRelease(toRelease);
     holder.withEncumbrancesForDelete(toDelete);
     return holder;
@@ -54,7 +55,26 @@ public class PendingToPendingEncumbranceStrategy implements EncumbranceWorkflowS
   private List<EncumbranceRelationsHolder> getTransactionsToDelete(List<EncumbranceRelationsHolder> encumbranceRelationsHolders) {
     return encumbranceRelationsHolders.stream()
       .filter(holder -> Objects.isNull(holder.getNewEncumbrance()))
-      .collect(toList());
+      .toList();
+  }
+
+  private List<EncumbranceRelationsHolder> getTransactionsToUpdate(List<EncumbranceRelationsHolder> encumbranceRelationsHolders) {
+    // Return a list of holders with transactions that need to have an expense class update.
+    // We want to avoid loading all financial data like in pending->open, so we copy some fields
+    // from the old encumbrance (amount is 0 with pending orders).
+    List<EncumbranceRelationsHolder> toUpdate = encumbranceRelationsHolders.stream()
+      .filter(holder -> holder.getNewEncumbrance() != null && holder.getOldEncumbrance() != null &&
+        !Objects.equals(holder.getNewEncumbrance().getExpenseClassId(), holder.getOldEncumbrance().getExpenseClassId()))
+      .toList();
+    toUpdate.forEach(holder -> {
+      Transaction oldEncumbrance = holder.getOldEncumbrance();
+      Transaction newEncumbrance = holder.getNewEncumbrance();
+      newEncumbrance.withFiscalYearId(oldEncumbrance.getFiscalYearId())
+        .withCurrency(oldEncumbrance.getCurrency())
+        .withAmount(oldEncumbrance.getAmount())
+        .getEncumbrance().withInitialAmountEncumbered(oldEncumbrance.getEncumbrance().getInitialAmountEncumbered());
+    });
+    return toUpdate;
   }
 
 }
