d0b7c025496a61b78adea6b0b902b89aaa29167b
Volodymyr_Rohach <Volodymyr_Rohach@epam.com>
2024-03-11 13:19:40 +0200

    MODORDERS-1061: Reverted to the old method-version.

--- src/main/java/org/folio/service/inventory/InventoryManager.java
+++ src/main/java/org/folio/service/inventory/InventoryManager.java
@@ -1270,30 +1270,14 @@ public class InventoryManager {
   }
 
   void updateItemWithPieceFields(Piece piece, JsonObject item) {
-    if (StringUtils.isNotEmpty(piece.getDisplaySummary())) {
-      item.put(ITEM_DISPLAY_SUMMARY, piece.getDisplaySummary());
-    }
-    if (StringUtils.isNotEmpty(piece.getEnumeration())) {
-      item.put(ITEM_ENUMERATION, piece.getEnumeration());
-    }
-    if (StringUtils.isNotEmpty(piece.getCopyNumber())) {
-      item.put(COPY_NUMBER, piece.getCopyNumber());
-    }
-    if (StringUtils.isNotEmpty(piece.getChronology())) {
-      item.put(ITEM_CHRONOLOGY, piece.getChronology());
-    }
-    if (StringUtils.isNotEmpty(piece.getBarcode())) {
-      item.put(ITEM_BARCODE, piece.getBarcode());
-    }
-    if (StringUtils.isNotEmpty(piece.getAccessionNumber())) {
-      item.put(ITEM_ACCESSION_NUMBER, piece.getAccessionNumber());
-    }
-    if (StringUtils.isNotEmpty(piece.getCallNumber())) {
-      item.put(ITEM_LEVEL_CALL_NUMBER, piece.getCallNumber());
-    }
-    if (piece.getDiscoverySuppress() != null) {
-      item.put(ITEM_DISCOVERY_SUPPRESS, piece.getDiscoverySuppress());
-    }
+    Optional.ofNullable(piece.getEnumeration())
+      .ifPresentOrElse(enumeration -> item.put(ITEM_ENUMERATION, enumeration), () -> item.remove(ITEM_ENUMERATION));
+    Optional.ofNullable(piece.getCopyNumber())
+      .ifPresentOrElse(copyNumber -> item.put(COPY_NUMBER, copyNumber), () -> item.remove(COPY_NUMBER));
+    Optional.ofNullable(piece.getChronology())
+      .ifPresentOrElse(chronology -> item.put(ITEM_CHRONOLOGY, chronology), () -> item.remove(ITEM_CHRONOLOGY));
+    Optional.ofNullable(piece.getDiscoverySuppress())
+      .ifPresentOrElse(discSup -> item.put(ITEM_DISCOVERY_SUPPRESS, discSup), () -> item.remove(ITEM_DISCOVERY_SUPPRESS));
   }
 
   public Future<SharingInstance> createShadowInstanceIfNeeded(String instanceId, RequestContext requestContext) {
