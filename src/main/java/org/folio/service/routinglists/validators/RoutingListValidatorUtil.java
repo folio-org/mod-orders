9f742a0f697f7c2182c2f090abe7e2537e2a65e6
saba_zedginidze <saba_zedginidze@epam.com>
2024-04-11 00:21:30 +0400

    [MODORDERS-1006] Fix future non-bloacking calls

--- src/main/java/org/folio/service/routinglists/validators/RoutingListValidatorUtil.java
+++ src/main/java/org/folio/service/routinglists/validators/RoutingListValidatorUtil.java
@@ -7,19 +7,20 @@ import org.folio.rest.core.exceptions.ErrorCodes;
 import org.folio.rest.jaxrs.model.Error;
 import org.folio.rest.jaxrs.model.Location;
 import org.folio.rest.jaxrs.model.PoLine;
+import org.folio.rest.jaxrs.model.RoutingList;
 import org.folio.rest.jaxrs.model.RoutingListCollection;
 
 public class RoutingListValidatorUtil {
 
   private RoutingListValidatorUtil() { }
 
-  public static List<Error> validateRoutingList(RoutingListCollection rListExisting, PoLine poLine) {
+  public static List<Error> validateRoutingList(RoutingList routingList, RoutingListCollection rListExisting, PoLine poLine) {
     List<ErrorCodes> errors = new ArrayList<>();
     if (poLine == null) {
       errors.add(ErrorCodes.PO_LINE_NOT_FOUND_FOR_ROUTING_LIST);
     } else if (!isPoLineFormatValid(poLine)) {
       errors.add(ErrorCodes.INVALID_ROUTING_LIST_FOR_PO_LINE_FORMAT);
-    } else if (isRoutingListsLimitReached(rListExisting, poLine)) {
+    } else if (isRoutingListsLimitReached(routingList, rListExisting, poLine)) {
       errors.add(ErrorCodes.ROUTING_LIST_LIMIT_REACHED_FOR_PO_LINE);
     }
     return errors.stream().map(ErrorCodes::toError).toList();
@@ -30,12 +31,19 @@ public class RoutingListValidatorUtil {
       || poLine.getOrderFormat() == PoLine.OrderFormat.P_E_MIX;
   }
 
-  private static boolean isRoutingListsLimitReached(RoutingListCollection rListExisting, PoLine poLine) {
-    return getQuantityPhysicalTotal(poLine) <= rListExisting.getTotalRecords();
+  private static boolean isRoutingListsLimitReached(RoutingList routingList, RoutingListCollection rListExisting, PoLine poLine) {
+    return getQuantityPhysicalTotal(poLine) <= extractDifferentRoutingLists(routingList, rListExisting).size();
   }
 
   private static int getQuantityPhysicalTotal(PoLine poLine) {
     return poLine.getLocations().stream().mapToInt(Location::getQuantityPhysical).sum();
+  }
+
+  private static List<RoutingList> extractDifferentRoutingLists(RoutingList routingList, RoutingListCollection routingListCollection) {
+    return routingListCollection.getRoutingLists()
+      .stream()
+      .filter(l -> !l.getId().equals(routingList.getId()))
+      .toList();
   }
 
 }
