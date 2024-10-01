037b9d9a0f7e1d4ac4585e5e3d5b1e12885a6b50
saba_zedginidze <saba_zedginidze@epam.com>
2024-10-01 15:34:49 +0400

    [MODORDERS-1174] Add SettingsRetrieverTest

--- src/main/java/org/folio/service/settings/SettingsRetriever.java
+++ src/main/java/org/folio/service/settings/SettingsRetriever.java
@@ -4,8 +4,11 @@ import com.github.benmanes.caffeine.cache.AsyncCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.rest.acq.model.Setting;
+import org.folio.rest.acq.model.SettingCollection;
 import org.folio.rest.core.RestClient;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.core.models.RequestEntry;
@@ -17,14 +20,13 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
-import static org.folio.orders.utils.CommonFields.COLLECTION_TOTAL;
 import static org.folio.orders.utils.ResourcePathResolver.ORDER_SETTINGS;
 import static org.folio.orders.utils.ResourcePathResolver.resourcesPath;
-import static org.folio.service.settings.util.SettingFields.COLLECTION_SETTINGS;
 import static org.folio.service.settings.util.SettingFields.KEY;
 
-@Log4j2
 public class SettingsRetriever {
+
+  private static final Logger logger = LogManager.getLogger(SettingsRetriever.class);
 
   private static final String SETTINGS_ENDPOINT = resourcesPath(ORDER_SETTINGS);
   private static final String SETTINGS_BY_KEY_QUERY = KEY.getValue() + "==%s";
@@ -50,25 +52,19 @@ public class SettingsRetriever {
       var cacheKey = String.format(SETTINGS_CACHE_KEY, TenantTool.tenantId(requestContext.getHeaders()), settingKey.getName());
       return Future.fromCompletionStage(asyncCache.get(cacheKey, (key, executor) -> getSettingByKeyFromRemote(settingKey, requestContext)));
     } catch (Exception e) {
-      log.error("Exception occurred when retrieving setting for key: {}", settingKey.getName(), e);
+      logger.error("Exception occurred when retrieving setting for key: {}", settingKey.getName(), e);
       return Future.failedFuture(e);
     }
   }
 
   private CompletableFuture<Optional<Setting>> getSettingByKeyFromRemote(SettingKey settingKey, RequestContext requestContext) {
-    RequestEntry requestEntry = new RequestEntry(SETTINGS_ENDPOINT)
-      .withQuery(String.format(SETTINGS_BY_KEY_QUERY, settingKey.getName()))
-      .withLimit(1)
-      .withOffset(0);
+    var requestEntry = new RequestEntry(SETTINGS_ENDPOINT).withLimit(1).withOffset(0)
+      .withQuery(String.format(SETTINGS_BY_KEY_QUERY, settingKey.getName()));
     return restClient.getAsJsonObject(requestEntry, requestContext)
-      .map(settings -> {
-        var totalRecords = settings.getInteger(COLLECTION_TOTAL.getValue());
-        var settingsArray = settings.getJsonArray(COLLECTION_SETTINGS.getValue());
-        if (totalRecords == null || totalRecords != 1 || settingsArray == null || settingsArray.isEmpty()) {
-          return Optional.<Setting>empty();
-        }
-        return Optional.of(settingsArray.getJsonObject(0).mapTo(Setting.class));
-      })
+      .map(settings -> settings.mapTo(SettingCollection.class))
+      .map(settings -> settings.getTotalRecords() == null || settings.getTotalRecords() != 1 || CollectionUtils.isEmpty(settings.getSettings())
+        ? Optional.<Setting>empty()
+        : Optional.of(settings.getSettings().get(0)))
       .toCompletionStage()
       .toCompletableFuture();
   }
