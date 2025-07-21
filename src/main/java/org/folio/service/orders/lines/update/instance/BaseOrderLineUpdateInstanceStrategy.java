d3c18e09246d9055b750bf99e202175329206130
saba_zedginidze <saba_zedginidze@epam.com>
2025-07-21 14:36:53 +0400

    Store deleted holding ids in the holder object and filter them out when getting unique locations

--- src/main/java/org/folio/service/orders/lines/update/instance/BaseOrderLineUpdateInstanceStrategy.java
+++ src/main/java/org/folio/service/orders/lines/update/instance/BaseOrderLineUpdateInstanceStrategy.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.models.orders.lines.update.OrderLineUpdateInstanceHolder;
-import org.folio.okapi.common.GenericCompositeFuture;
 import org.folio.orders.utils.RequestContextUtil;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.jaxrs.model.PoLine;
@@ -13,6 +12,10 @@ import org.folio.service.inventory.InventoryHoldingManager;
 import org.folio.service.inventory.InventoryInstanceManager;
 import org.folio.service.inventory.InventoryItemManager;
 import org.folio.service.orders.lines.update.OrderLineUpdateInstanceStrategy;
+
+import java.util.List;
+
+import static org.folio.orders.utils.HelperUtils.collectResultsOnSuccessNonNull;
 
 public abstract class BaseOrderLineUpdateInstanceStrategy implements OrderLineUpdateInstanceStrategy {
 
@@ -38,34 +41,29 @@ public abstract class BaseOrderLineUpdateInstanceStrategy implements OrderLineUp
     return processHoldings(holder, requestContext);
   }
 
-  abstract Future<Void> processHoldings(OrderLineUpdateInstanceHolder holder, RequestContext requestContext);
-
-  Future<Void> deleteAbandonedHoldings(boolean isDeleteAbandonedHoldings, PoLine poLine, RequestContext requestContext) {
+  Future<List<String>> deleteAbandonedHoldings(boolean isDeleteAbandonedHoldings, PoLine poLine, RequestContext requestContext) {
     if (!isDeleteAbandonedHoldings) {
       return Future.succeededFuture();
     }
-    return GenericCompositeFuture.join(
-        poLine.getLocations()
-          .stream()
-          .filter(location -> StringUtils.isNotEmpty(location.getHoldingId()))
-          .map(location -> {
-            var locationContext = RequestContextUtil.createContextWithNewTenantId(requestContext, location.getTenantId());
-            return deleteHoldingWithoutItems(location.getHoldingId(), locationContext)
-              .onSuccess(v -> logger.info("deleteAbandonedHoldings:: operation succeeded for holdingId: {}", location.getHoldingId()))
-              .onFailure(e -> logger.error("Failed to delete abandoned holdings for holdingId: {}", location.getHoldingId(), e));
-          }).toList()
-      )
-      .mapEmpty();
+    var deleteHoldingFutures = poLine.getLocations().stream()
+      .filter(location -> StringUtils.isNotEmpty(location.getHoldingId()))
+      .map(location -> {
+        var locationContext = RequestContextUtil.createContextWithNewTenantId(requestContext, location.getTenantId());
+        return deleteHoldingWithoutItems(location.getHoldingId(), locationContext)
+          .onSuccess(v -> logger.info("deleteAbandonedHoldings:: operation succeeded for holdingId: {}", location.getHoldingId()))
+          .onFailure(e -> logger.error("Failed to delete abandoned holdings for holdingId: {}", location.getHoldingId(), e))
+          .map(deleted -> deleted ? location.getHoldingId() : null);
+      }).toList();
+    return collectResultsOnSuccessNonNull(deleteHoldingFutures);
   }
 
-  private Future<Void> deleteHoldingWithoutItems(String holdingId, RequestContext requestContext) {
+  private Future<Boolean> deleteHoldingWithoutItems(String holdingId, RequestContext requestContext) {
     return inventoryItemManager.getItemsByHoldingId(holdingId, requestContext)
-      .compose(items -> {
-        if (items.isEmpty()) {
-          return inventoryHoldingManager.deleteHoldingById(holdingId, true, requestContext);
-        } else {
-          return Future.succeededFuture();
-        }
-      });
+      .compose(items -> items.isEmpty()
+        ? inventoryHoldingManager.deleteHoldingById(holdingId, true, requestContext).map(true)
+        : Future.succeededFuture(false));
   }
+
+  abstract Future<Void> processHoldings(OrderLineUpdateInstanceHolder holder, RequestContext requestContext);
+
 }
