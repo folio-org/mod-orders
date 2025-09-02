ea50dbf1ad84903a9965fe66a52f6a792d6f7163
Ruslan Lavrov <ruslan_lavrov@epam.com>
2025-09-02 13:16:16 +0300

    Added cancelled jobs cache

---
+++ src/test/java/org/folio/service/caches/CancelledJobsIdsCacheTest.java
@@ -0,0 +1,41 @@
+package org.folio.service.caches;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CancelledJobsIdsCacheTest {
+
+  public static final long CACHE_EXPIRATION_TIME_MINS = 5;
+
+  private CancelledJobsIdsCache cache;
+
+  @BeforeEach
+  void setUp() {
+    cache = new CancelledJobsIdsCache(CACHE_EXPIRATION_TIME_MINS);
+  }
+
+  @Test
+  void shouldIdAddToCache() {
+    UUID jobId = UUID.randomUUID();
+    cache.put(jobId);
+    assertTrue(cache.contains(jobId));
+  }
+
+  @Test
+  void shouldReturnFalseForNonExistentId() {
+    UUID jobId = UUID.randomUUID();
+    assertFalse(cache.contains(jobId));
+  }
+
+  @Test
+  void shouldThrowExceptionIfJobIdIsNull() {
+    assertThrows(NullPointerException.class, () -> cache.contains(null));
+  }
+
+}
