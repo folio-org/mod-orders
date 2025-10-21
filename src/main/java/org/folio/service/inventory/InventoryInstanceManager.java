535987d661bf3775769aae907a52146cf4515938
saba_zedginidze <saba_zedginidze@epam.com>
2025-10-21 17:01:39 +0400

    [MODORDERS-1369] Remove unused import

--- src/main/java/org/folio/service/inventory/InventoryInstanceManager.java
+++ src/main/java/org/folio/service/inventory/InventoryInstanceManager.java
@@ -36,7 +36,6 @@ import java.util.concurrent.CompletionException;
 import java.util.stream.Stream;
 
 import static java.util.Collections.singletonList;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.folio.orders.utils.HelperUtils.collectResultsOnSuccess;
