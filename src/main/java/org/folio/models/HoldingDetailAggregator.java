d728de1cbda45dd0936a9f2c0585ee45aadaaa82
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-02-13 15:18:53 +0500

    [MODORDERS-1403]. Enhance the holding-detail endpoint to always return all linked PO lines

---
+++ src/main/java/org/folio/models/HoldingDetailAggregator.java
@@ -0,0 +1,52 @@
+package org.folio.models;
+
+import io.vertx.core.json.JsonObject;
+import org.apache.commons.lang3.StringUtils;
+import org.folio.rest.jaxrs.model.Piece;
+import org.folio.rest.jaxrs.model.PoLine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class HoldingDetailAggregator {
+  private Map<String, List<PoLine>> poLinesByHoldingId = new HashMap<>();
+  private Map<String, List<Piece>> piecesByHoldingId = new HashMap<>();
+  private Map<String, List<JsonObject>> itemsByHoldingId = new HashMap<>();
+
+  public Map<String, List<PoLine>> getPoLinesByHoldingId() {
+    return poLinesByHoldingId;
+  }
+
+  public Map<String, List<Piece>>  getPiecesByHoldingId() {
+    return piecesByHoldingId;
+  }
+
+  public Map<String, List<JsonObject>>  getItemsByHoldingId() {
+    return itemsByHoldingId;
+  }
+
+  public String getPieceTenantIdByItemId(String itemId) {
+    return piecesByHoldingId.values().stream()
+      .flatMap(List::stream)
+      .filter(Objects::nonNull)
+      .filter(piece -> StringUtils.equals(itemId, piece.getItemId()))
+      .map(Piece::getReceivingTenantId)
+      .filter(Objects::nonNull)
+      .findFirst()
+      .orElse(null);
+  }
+
+  public void setPoLinesByHoldingId(Map<String, List<PoLine>> poLines) {
+    this.poLinesByHoldingId = poLines;
+  }
+
+  public void setPiecesByHoldingId(Map<String, List<Piece>> pieces) {
+    this.piecesByHoldingId = pieces;
+  }
+
+  public void setItemsByHoldingId(Map<String, List<JsonObject>> items) {
+    this.itemsByHoldingId = items;
+  }
+}
--- src/main/java/org/folio/service/orders/HoldingDetailService.java
+++ src/main/java/org/folio/service/orders/HoldingDetailService.java
@@ -4,9 +4,7 @@ import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang.StringUtils;
-import org.folio.models.HoldingDetailHolder;
+import org.folio.models.HoldingDetailAggregator;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.jaxrs.model.HoldingDetailResults;
 import org.folio.rest.jaxrs.model.HoldingDetailResultsProperty;
@@ -21,16 +19,15 @@ import org.folio.rest.jaxrs.model.PoLinesDetailCollection;
 import org.folio.service.inventory.InventoryItemManager;
 import org.folio.service.pieces.PieceStorageService;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static org.folio.orders.utils.HelperUtils.collectResultsOnSuccess;
-import static org.folio.orders.utils.RequestContextUtil.createContextWithNewTenantId;
 import static org.folio.service.inventory.InventoryItemManager.ID;
+import static org.folio.service.inventory.InventoryItemManager.ITEM_HOLDINGS_RECORD_ID;
 
 @Log4j2
 public class HoldingDetailService {
@@ -52,187 +49,125 @@ public class HoldingDetailService {
       log.info("postOrdersHoldingDetail:: No holding ids were passed");
       return Future.succeededFuture(new HoldingDetailResults());
     }
-    return purchaseOrderLineService.getPoLinesByHoldingIds(holdingIds, requestContext)
-      .compose(poLines -> {
-        if (CollectionUtils.isEmpty(poLines)) {
-          log.info("postOrdersHoldingDetail:: No poLines were found by holding ids={}", holdingIds);
-          return Future.succeededFuture(new HoldingDetailResults());
-        }
-        var poLineIds = poLines.stream().map(PoLine::getId).distinct().toList();
-        return pieceStorageService.getPiecesByLineIdsByChunks(poLineIds, requestContext)
-          .compose(pieces -> {
-            if (CollectionUtils.isEmpty(pieces)) {
-              log.info("postOrdersHoldingDetail:: No pieces were found by holding ids={}, poLine ids={}", holdingIds, poLineIds);
-              var groupedPoLinesDetailsByHoldingId = createGroupedPoLineDetailsByHoldingId(holdingIds, poLines);
-              return Future.succeededFuture(createHoldingDetailsResultsWithPoLines(groupedPoLinesDetailsByHoldingId));
-            }
-            var holdersFutures = new ArrayList<Future<HoldingDetailHolder>>();
-            groupPiecesByTenantIdAndHoldingId(pieces)
-              .forEach((tenantId, groupedPiecesByHoldingId) -> {
-                if (MapUtils.isNotEmpty(groupedPiecesByHoldingId)) {
-                  groupedPiecesByHoldingId.forEach((holdingId, groupedPieces) -> {
-                    log.info("postOrdersHoldingDetail:: Processing holding detail by tenant={}, holding id={}, pieces={}", getTenantId(tenantId), holdingId, groupedPieces.size());
-                    var poLinesDetails = createPoLineDetailFromPieces(groupedPieces);
-                    var piecesDetail = createPieceDetail(groupedPieces);
-                    var holdersFuture = getHolderFuture(tenantId, holdingId, poLinesDetails, piecesDetail, requestContext);
-                    holdersFutures.add(holdersFuture);
-                  });
-                }
-              });
-            return collectResultsOnSuccess(holdersFutures)
-              .map(this::createHoldingDetailResults);
-        });
+    var aggregator = new HoldingDetailAggregator();
+
+    // Retrieve poLines, pieces, and items independently in parallel
+    var poLinesFuture = purchaseOrderLineService.getPoLinesByHoldingIds(holdingIds, requestContext)
+      .map(poLines -> {
+        log.info("postOrdersHoldingDetail:: Found {} poLines by holding ids={}", poLines.size(), holdingIds);
+        var poLinesByHoldingIds = groupedPoLinesByHoldingId(holdingIds, poLines);
+        aggregator.setPoLinesByHoldingId(poLinesByHoldingIds);
+        return null;
       })
       .recover(throwable -> {
-        log.error("postOrdersHoldingDetail:: Error processing holding details for holding ids={}", holdingIds, throwable);
+        log.warn("postOrdersHoldingDetail:: Failed to retrieve poLines for holding ids={}", holdingIds, throwable);
+        aggregator.setPoLinesByHoldingId(Collections.emptyMap());
+        return Future.succeededFuture(null);
+      });
+
+    var piecesFuture = pieceStorageService.getPiecesByHoldingIds(holdingIds, requestContext)
+      .map(pieces -> {
+        log.info("postOrdersHoldingDetail:: Found {} pieces by holding ids={}", pieces.size(), holdingIds);
+        var piecesByHoldingId = groupPiecesByHoldingId(pieces);
+        aggregator.setPiecesByHoldingId(piecesByHoldingId);
+        return null;
+      })
+      .recover(throwable -> {
+        log.warn("postOrdersHoldingDetail:: Failed to retrieve pieces for holding ids={}", holdingIds, throwable);
+        aggregator.setPiecesByHoldingId(Collections.emptyMap());
+        return Future.succeededFuture(null);
+      });
+
+    var itemsFuture = inventoryItemManager.getItemsByHoldingIds(holdingIds, requestContext)
+      .map(items -> {
+        log.info("postOrdersHoldingDetail:: Found {} items by holding ids={}", items.size(), holdingIds);
+        var itemsByHoldingId = groupItemsByHoldingId(items);
+        aggregator.setItemsByHoldingId(itemsByHoldingId);
+        return null;
+      })
+      .recover(throwable -> {
+        log.warn("postOrdersHoldingDetail:: Failed to retrieve items for holding ids={}", holdingIds, throwable);
+        aggregator.setItemsByHoldingId(Collections.emptyMap());
+        return Future.succeededFuture(null);
+      });
+
+    return Future.all(poLinesFuture, piecesFuture, itemsFuture)
+      .compose(v -> {
+        var holdingDetailResults = new HoldingDetailResults();
+        holdingIds.forEach(holdingId -> {
+          var poLinesDetailCollection = new PoLinesDetailCollection();
+          var piecesDetailCollection = new PiecesDetailCollection();
+          var itemsDetailCollection = new ItemsDetailCollection();
+
+          if (aggregator.getPoLinesByHoldingId().containsKey(holdingId)) {
+            var poLinesDetails = aggregator.getPoLinesByHoldingId().get(holdingId).stream()
+              .filter(Objects::nonNull)
+              .map(poLine -> new PoLinesDetail().withId(poLine.getId()))
+              .toList();
+            poLinesDetailCollection.withPoLinesDetail(poLinesDetails)
+              .withTotalRecords(poLinesDetails.size());
+          }
+          if (aggregator.getPiecesByHoldingId().containsKey(holdingId)) {
+            var piecesDetails = aggregator.getPiecesByHoldingId().get(holdingId).stream()
+              .filter(Objects::nonNull)
+              .map(piece -> new PiecesDetail().withId(piece.getId())
+                .withPoLineId(piece.getPoLineId())
+                .withItemId(piece.getItemId())
+                .withTenantId(piece.getReceivingTenantId()))
+              .toList();
+            piecesDetailCollection.withPiecesDetail(piecesDetails)
+              .withTotalRecords(piecesDetails.size());
+          }
+          if (aggregator.getItemsByHoldingId().containsKey(holdingId)) {
+            var itemsDetails = aggregator.getItemsByHoldingId().get(holdingId).stream()
+              .filter(Objects::nonNull)
+              .map(item -> new ItemsDetail().withId(item.getString(ID))
+                .withTenantId(aggregator.getPieceTenantIdByItemId(item.getString(ID))))
+              .toList();
+            itemsDetailCollection.withItemsDetail(itemsDetails)
+              .withTotalRecords(itemsDetails.size());
+          }
+          var holdingDetailProperty = new HoldingDetailResultsProperty()
+            .withPoLinesDetailCollection(poLinesDetailCollection)
+            .withPiecesDetailCollection(piecesDetailCollection)
+            .withItemsDetailCollection(itemsDetailCollection);
+          holdingDetailResults.withAdditionalProperty(holdingId, holdingDetailProperty);
+        });
+        return Future.succeededFuture(holdingDetailResults);
+      })
+      .recover(throwable -> {
+        log.error("postOrdersHoldingDetail:: Error building holding detail results for holding ids={}", holdingIds, throwable);
         return Future.failedFuture(throwable);
       });
   }
 
-  protected Map<String, Map<String, List<Piece>>> groupPiecesByTenantIdAndHoldingId(List<Piece> pieces) {
-    if (CollectionUtils.isEmpty(pieces)) {
-      return Collections.emptyMap();
-    }
-    return pieces.stream()
-      .filter(Objects::nonNull)
-      .filter(piece -> Objects.nonNull(piece.getHoldingId()))
-      .map(this::getPieceWithNonNullReceivingTenant)
-      .collect(Collectors.groupingBy(Piece::getReceivingTenantId, Collectors.groupingBy(Piece::getHoldingId)));
-  }
-
-  private Piece getPieceWithNonNullReceivingTenant(Piece piece) {
-    return Objects.nonNull(piece.getReceivingTenantId()) ? piece : piece.withReceivingTenantId("");
-  }
-
-  protected Future<HoldingDetailHolder> getHolderFuture(String tenantId, String holdingId, List<PoLinesDetail> poLinesDetails,
-                                                        List<PiecesDetail> piecesDetail, RequestContext requestContext) {
-    var localRequestContext = StringUtils.isNotBlank(tenantId) ? createContextWithNewTenantId(requestContext, tenantId) : requestContext;
-    return inventoryItemManager.getItemsByHoldingId(holdingId, localRequestContext)
-      .map(items -> {
-        if (CollectionUtils.isEmpty(items)) {
-          return Collections.<ItemsDetail>emptyList();
-        }
-        return items.stream()
-          .filter(Objects::nonNull)
-          .map(item -> createItemDetail(tenantId, item))
-          .toList();
-      })
-      .map(itemsDetail -> {
-        var finalPoLinesDetail = CollectionUtils.isNotEmpty(poLinesDetails) ? poLinesDetails : Collections.<PoLinesDetail>emptyList();
-        var finalPiecesDetail = CollectionUtils.isNotEmpty(piecesDetail) ? piecesDetail : Collections.<PiecesDetail>emptyList();
-        return new HoldingDetailHolder(holdingId, finalPoLinesDetail, finalPiecesDetail, itemsDetail);
-      });
-  }
-
-  private Map<String, List<PoLinesDetail>> createGroupedPoLineDetailsByHoldingId(List<String> holdingIds, List<PoLine> poLines) {
+  private Map<String, List<PoLine>> groupedPoLinesByHoldingId(List<String> holdingIds, List<PoLine> poLines) {
     if (CollectionUtils.isEmpty(poLines)) {
       return Collections.emptyMap();
     }
-    var holdingIdSet = CollectionUtils.isEmpty(holdingIds) ? Collections.emptySet() : new java.util.HashSet<>(holdingIds);
+    var holdingIdSet = new HashSet<>(holdingIds);
     return poLines.stream()
       .filter(Objects::nonNull)
       .filter(poLine -> CollectionUtils.isNotEmpty(poLine.getLocations()))
       .flatMap(poLine -> poLine.getLocations().stream()
         .filter(location -> Objects.nonNull(location.getHoldingId()))
         .filter(location -> holdingIdSet.contains(location.getHoldingId()))
-        .map(location -> Map.entry(location.getHoldingId(), poLine.getId())))
+        .map(location -> Map.entry(location.getHoldingId(), poLine)))
       .distinct()
-      .collect(Collectors.groupingBy(
-        Map.Entry::getKey,
-        Collectors.mapping(
-          entry -> new PoLinesDetail().withId(entry.getValue()),
-          Collectors.toList()
-        )
-      ));
+      .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
   }
 
-  private List<PoLinesDetail> createPoLineDetailFromPieces(List<Piece> groupedPieces) {
-    if (CollectionUtils.isEmpty(groupedPieces)) {
-      return Collections.emptyList();
-    }
-    return groupedPieces.stream()
+  private Map<String, List<Piece>> groupPiecesByHoldingId(List<Piece> pieces) {
+    return pieces.stream()
       .filter(Objects::nonNull)
-      .map(Piece::getPoLineId)
-      .distinct()
-      .map(poLineId -> new PoLinesDetail().withId(poLineId))
-      .toList();
+      .filter(piece -> Objects.nonNull(piece.getHoldingId()))
+      .collect(Collectors.groupingBy(Piece::getHoldingId));
   }
 
-  private List<PiecesDetail> createPieceDetail(List<Piece> groupedPieces) {
-    if (CollectionUtils.isEmpty(groupedPieces)) {
-      return Collections.emptyList();
-    }
-    return groupedPieces.stream()
+  private Map<String, List<JsonObject>> groupItemsByHoldingId(List<JsonObject> items) {
+    return items.stream()
       .filter(Objects::nonNull)
-      .map(piece -> new PiecesDetail()
-        .withId(piece.getId())
-        .withPoLineId(piece.getPoLineId())
-        .withItemId(piece.getItemId())
-        .withTenantId(getTenantId(piece.getReceivingTenantId())))
-      .toList();
-  }
-
-  private ItemsDetail createItemDetail(String tenantId, JsonObject item) {
-    return new ItemsDetail()
-      .withId(item.getString(ID))
-      .withTenantId(getTenantId(tenantId));
-  }
-
-  private String getTenantId(String tenantId) {
-    return StringUtils.isNotBlank(tenantId) ? tenantId : null;
-  }
-
-  private HoldingDetailResults createHoldingDetailsResultsWithPoLines(Map<String, List<PoLinesDetail>> groupedPoLinesDetailsByHoldingId) {
-    if (MapUtils.isEmpty(groupedPoLinesDetailsByHoldingId)) {
-      log.info("createHoldingDetailsResultsWithPoLines:: No detail holders were generated");
-      return new HoldingDetailResults();
-    }
-    var holdingDetailResults = new HoldingDetailResults();
-    groupedPoLinesDetailsByHoldingId.forEach((holdingId, poLinesDetails) -> {
-      var poLinesDetailCollection = new PoLinesDetailCollection();
-      var piecesDetailCollection = new PiecesDetailCollection();
-      var itemsDetailCollection = new ItemsDetailCollection();
-      if (CollectionUtils.isNotEmpty(poLinesDetails)) {
-        poLinesDetailCollection.withPoLinesDetail(poLinesDetails)
-          .withTotalRecords(poLinesDetails.size());
-      }
-      var holdingDetailProperty = new HoldingDetailResultsProperty()
-        .withPoLinesDetailCollection(poLinesDetailCollection)
-        .withPiecesDetailCollection(piecesDetailCollection)
-        .withItemsDetailCollection(itemsDetailCollection);
-      holdingDetailResults.withAdditionalProperty(holdingId, holdingDetailProperty);
-    });
-    return holdingDetailResults;
-  }
-
-  private HoldingDetailResults createHoldingDetailResults(List<HoldingDetailHolder> holders) {
-    if (CollectionUtils.isEmpty(holders)) {
-      log.info("createHoldingDetailResults:: No detail holders were generated");
-      return new HoldingDetailResults();
-    }
-    var holdingDetailResults = new HoldingDetailResults();
-    holders.forEach(holder -> {
-      var poLinesDetailCollection = new PoLinesDetailCollection();
-      var piecesDetailCollection = new PiecesDetailCollection();
-      var itemsDetailCollection = new ItemsDetailCollection();
-      if (CollectionUtils.isNotEmpty(holder.poLines())) {
-        poLinesDetailCollection.withPoLinesDetail(holder.poLines())
-          .withTotalRecords(holder.poLines().size());
-      }
-      if (CollectionUtils.isNotEmpty(holder.pieces())) {
-        piecesDetailCollection.withPiecesDetail(holder.pieces())
-          .withTotalRecords(holder.pieces().size());
-      }
-      if (CollectionUtils.isNotEmpty(holder.items())) {
-        itemsDetailCollection.withItemsDetail(holder.items())
-          .withTotalRecords(holder.items().size());
-      }
-      var holdingDetailProperty = new HoldingDetailResultsProperty()
-        .withPoLinesDetailCollection(poLinesDetailCollection)
-        .withPiecesDetailCollection(piecesDetailCollection)
-        .withItemsDetailCollection(itemsDetailCollection);
-      holdingDetailResults.withAdditionalProperty(holder.holdingId(), holdingDetailProperty);
-    });
-    return holdingDetailResults;
+      .filter(item -> Objects.nonNull(item.getString(ITEM_HOLDINGS_RECORD_ID)))
+      .collect(Collectors.groupingBy(item -> item.getString(ITEM_HOLDINGS_RECORD_ID)));
   }
 }
