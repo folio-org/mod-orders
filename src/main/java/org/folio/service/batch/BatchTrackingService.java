51dc99e0c0591c068ce1b74da475ff7060b98d75
saba_zedginidze <saba_zedginidze@epam.com>
2026-02-22 13:05:56 +0400

    [MODORDSTOR-501] Populate batch ID header correctly

--- src/main/java/org/folio/service/batch/BatchTrackingService.java
+++ src/main/java/org/folio/service/batch/BatchTrackingService.java
@@ -8,6 +8,9 @@ import org.folio.rest.core.RestClient;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.core.models.RequestEntry;
 
+import java.util.HashMap;
+
+import static org.folio.orders.utils.FutureUtils.asFuture;
 import static org.folio.orders.utils.ResourcePathResolver.BATCH_TRACKING;
 import static org.folio.orders.utils.ResourcePathResolver.resourcesPath;
 
@@ -24,10 +27,21 @@ public class BatchTrackingService {
     var batchTracking = new BatchTracking().withId(id).withTotalRecords(totalRecords);
     var requestEntry = new RequestEntry(BATCH_TRACKING_ENDPOINT);
     return restClient.post(requestEntry, batchTracking, BatchTracking.class, requestContext)
-      .map(tracking -> requestContext.getHeaders().put(BATCH_TRACKING_HEADER, tracking.getId())) // Add the batch tracking ID to the request headers for downstream services to use.
+      .map(tracking -> asFuture(() -> populateBatchId(tracking.getId(), requestContext))) // Add the batch tracking ID to the request headers for downstream services to use.
       .onFailure(t -> log.error("Failed to create batch tracking record for batchId: {}", batchTracking.getId(), t))
       .recover(t -> Future.succeededFuture()) // In case of failure, we return a succeeded future to avoid failing the entire batch process.
       .mapEmpty();
+  }
+
+  /**
+   Populate the batch id in the request headers
+
+   @param requestContext the request context containing the headers
+   */
+  public static void populateBatchId(String batchId, RequestContext requestContext) {
+    var headers = new HashMap<>(requestContext.getHeaders());
+    headers.put(BATCH_TRACKING_HEADER, batchId);
+    requestContext.withHeaders(headers);
   }
 
 }
