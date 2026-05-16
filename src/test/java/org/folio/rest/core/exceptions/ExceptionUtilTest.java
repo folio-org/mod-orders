901f31d8b4e303f98104705ee8e6b39757470162
aitestmate <ai_testmate@epam.com>
2026-05-16 01:04:30 +0000

    [chore] [mod-orders] unit tests

--- src/test/java/org/folio/rest/core/exceptions/ExceptionUtilTest.java
+++ src/test/java/org/folio/rest/core/exceptions/ExceptionUtilTest.java
@@ -15,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import io.vertx.pgclient.PgException;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class ExceptionUtilTest {
 
@@ -110,5 +112,92 @@ public class ExceptionUtilTest {
     String errorMsg = "{\"message\":\"Test\",\"code\":\"Test\",\"parameters\":[]}";
     boolean act = ExceptionUtil.isAffiliationMissedError(errorMsg);
     assertFalse(act);
+  }
+
+    @ParameterizedTest
+  @NullAndEmptySource
+  void testIsAffiliationMissedWhenEmptyShouldReturnFalse(String errorMessage) {
+    // TestMate-2bb210f8584f90e17dd9d2c09163579c
+    // Given
+    // errorMessage is null or empty
+    // When
+    boolean act = ExceptionUtil.isAffiliationMissedError(errorMessage);
+    // Then
+    assertFalse(act);
+  }
+
+    @Test
+  void testGetHttpExceptionWhenErrorIsJsonErrorsObject() {
+    // TestMate-e0f5d081e68f167412e62853b0b998ac
+    // Given
+    int statusCode = 422;
+    String errorJson = "{\"errors\": [{\"message\": \"msg\", \"code\": \"code\", \"parameters\": []}], \"total_records\": 1}";
+    // When
+    HttpException httpException = ExceptionUtil.getHttpException(statusCode, errorJson);
+    // Then
+    assertEquals(422, httpException.getCode());
+    Errors actualErrors = httpException.getErrors();
+    assertEquals(1, actualErrors.getTotalRecords());
+    List<Error> errorList = actualErrors.getErrors();
+    assertEquals(1, errorList.size());
+    assertEquals("code", errorList.get(0).getCode());
+    assertEquals("msg", errorList.get(0).getMessage());
+  }
+
+    @Test
+  void testGetHttpExceptionWhenErrorIsJsonErrorsObjectWithFormatting() {
+    // TestMate-a0fb16b812b87bd16c84479f77598570
+    // Given
+    int statusCode = 400;
+    String formattedErrorJson = "{\n \"errors\" : [ {\r\n \"message\": \"msg\", \"code\": \"code\", \"parameters\": [] } ]\n, \"total_records\": 1}";
+    // When
+    HttpException httpException = ExceptionUtil.getHttpException(statusCode, formattedErrorJson);
+    // Then
+    assertEquals(400, httpException.getCode());
+    Errors actualErrors = httpException.getErrors();
+    assertEquals(1, actualErrors.getTotalRecords());
+    List<Error> errorList = actualErrors.getErrors();
+    assertEquals(1, errorList.size());
+    assertEquals("code", errorList.get(0).getCode());
+    assertEquals("msg", errorList.get(0).getMessage());
+  }
+
+    @Test
+  void testGetHttpExceptionWhenErrorIsEmpty() {
+    // TestMate-ab949d92af727354fce5bdc95d854fb8
+    // Given
+    int statusCode = 400;
+    String error = "";
+    // When
+    HttpException httpException = ExceptionUtil.getHttpException(statusCode, error);
+    // Then
+    assertEquals(400, httpException.getCode());
+    Errors actualErrors = httpException.getErrors();
+    assertEquals(1, actualErrors.getTotalRecords());
+    List<Error> errorList = actualErrors.getErrors();
+    assertEquals(1, errorList.size());
+    assertEquals(GENERIC_ERROR_CODE.getCode(), errorList.get(0).getCode());
+    // Based on HttpException(int code, String message) constructor implementation,
+    // the Error object in the list preserves the empty string message passed to it,
+    // even though the exception's super message defaults to the generic description.
+    assertEquals("", errorList.get(0).getMessage());
+  }
+
+    @Test
+  void testGetHttpExceptionWhenErrorIsMalformedJsonErrors() {
+    // TestMate-4eda9f7e475bc0c5401d2a94dfc7111b
+    // Given
+    int statusCode = 400;
+    String jsonMissingParameters = "{\"errors\": [{\"message\": \"msg\", \"code\": \"code\"}], \"total_records\": 1}";
+    // When
+    HttpException httpException = ExceptionUtil.getHttpException(statusCode, jsonMissingParameters);
+    // Then
+    assertEquals(400, httpException.getCode());
+    Errors actualErrors = httpException.getErrors();
+    assertEquals(1, actualErrors.getTotalRecords());
+    List<Error> errorList = actualErrors.getErrors();
+    assertEquals(1, errorList.size());
+    assertEquals(GENERIC_ERROR_CODE.getCode(), errorList.get(0).getCode());
+    assertEquals(jsonMissingParameters, errorList.get(0).getMessage());
   }
 }
