43d3718530a25772a59d127c7d67248dd680f0a9
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-05-19 14:32:25 +0500

    [MODORDERS-1443]. Abandoned holding is not deleted after changing instance connection if related piece exists (#1313)
    * [MODORDERS-1443]. Abandoned holding is not deleted after changing instance connection if related piece exists
    
    * [MODORDERS-1443]. Update log markers & comments
    
    (cherry picked from commit 2c2e693b86ca552614386a7cb1b75637106e474e)

--- src/main/java/org/folio/models/HoldingDataExclusionConfig.java
+++ src/main/java/org/folio/models/HoldingDataExclusionConfig.java
@@ -7,9 +7,10 @@ public record HoldingDataExclusionConfig(HoldingDataExclusionMode mode,
                                          Set<String> pendingPoLineIds,
                                          Set<String> excludePieceIds) {
 
+  // Must only be used by PURCHASE_ORDER_UNOPEN mode, because it is not concerned with Pieces
   public HoldingDataExclusionConfig(HoldingDataExclusionMode mode,
-                             Set<String> excludePoLinesIds,
-                             Set<String> pendingPoLineIds) {
+                                    Set<String> excludePoLinesIds,
+                                    Set<String> pendingPoLineIds) {
     this(mode, excludePoLinesIds, pendingPoLineIds, Set.of());
   }
 }
